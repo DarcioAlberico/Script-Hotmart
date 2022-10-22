bdf767395deb6979021ce461d7875f0c4ffe4897
Kartatz <105828205+Kartatz@users.noreply.github.com>
2022-10-22 12:39:05 -0300

    Initial commit

---
+++ src/symbols.h
@@ -0,0 +1,13 @@
+static const char DOT[] = ".";
+static const char SLASH[] = "/";
+static const char SPACE[] = " ";
+static const char COLON[] = ":";
+static const char QUOTATION_MARK[] = "\"";
+static const char AND[] = "&";
+static const char EQUAL[] = "=";
+
+#ifdef _WIN32
+	static const char PATH_SEPARATOR[] = "\\";
+#else
+	static const char PATH_SEPARATOR[] = "/";
+#endif
